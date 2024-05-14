d4cbfa9f4fe89b8cbe688330e38d2d048be6fc65
Pzzzs <2213514648@qq.com>
2024-05-14 14:52:25 +0800

    增加 预约辅导

--- FrontEnd/src/views/HomeView.vue
+++ FrontEnd/src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <el-container>
       <el-aside :width="asideWidth">
         <div style="height: 60px; line-height: 60px; font-size: 20px; display: flex; align-items: center; justify-content: center">
-          <img src="@/assets/logo.png" style="width: 30px;" alt="">
+          <img src="@/assets/nkd.jpg" style="width: 30px;" alt="">
           <span class="logo-title" v-show="!isCollapse">学术助手</span>
         </div>
         <el-menu router :collapse="isCollapse" :collapse-transition="false" background-color="#001529"
@@ -40,8 +40,8 @@
             <i class="el-icon-quanping" @click="handleFullScreen" style="font-size: 25px"></i>
             <el-dropdown placement="bottom">
               <div style="display: flex; align-items: center; cursor: pointer">
-                <img src="@/assets/logo.png" alt="" style="width: 40px; height: 40px; margin: 0 5px">
-                <span>管理员</span>
+                <img src="@/assets/nkd.jpg" alt="" style="width: 40px; height: 40px; margin: 0 5px">
+                <span>用户中心</span>
               </div>
               <el-dropdown-menu slot="dropdown">
                 <el-dropdown-item>个人信息</el-dropdown-item>
